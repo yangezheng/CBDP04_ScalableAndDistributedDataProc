cc08928a29d9a1cf76804ac4b6ef1c1dde4ed564
Tobias Goetz <goetzt@in.tum.de>
2023-11-30 15:41:53 +0100

    add sample solution

--- coordinator.cpp
+++ coordinator.cpp
@@ -1,9 +1,64 @@
 #include "CurlEasyPtr.h"
+#include <array>
+#include <charconv>
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <unordered_map>
+#include <vector>
+#include <netdb.h>
+#include <sys/poll.h>
+#include <unistd.h>
 
-using namespace std::literals;
+// Return a listening socket
+int getListenerSocket(char* port) {
+   addrinfo hints = {};
+   hints.ai_flags = AI_PASSIVE;
+   hints.ai_family = AF_INET6;
+   hints.ai_socktype = SOCK_STREAM;
+
+   // Get us a socket and bind it
+   addrinfo* aInfo = nullptr;
+   if (auto status = getaddrinfo(nullptr, port, &hints, &aInfo); status != 0) {
+      std::cerr << "getaddrinfo() failed: " << gai_strerror(status) << std::endl;
+      exit(1);
+   }
+
+   int listener;
+   addrinfo* iter;
+   for (iter = aInfo; iter; iter = iter->ai_next) {
+      listener = socket(iter->ai_family, iter->ai_socktype, iter->ai_protocol);
+      if (listener < 0)
+         continue;
+
+      // Lose the pesky "address already in use" error message
+      int optval = 1;
+      setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int));
+
+      if (bind(listener, iter->ai_addr, iter->ai_addrlen) < 0) {
+         close(listener);
+         continue;
+      }
+
+      break;
+   }
+
+   freeaddrinfo(aInfo); // All done with this
+
+   // If we got here, it means we didn't get bound
+   if (!iter) {
+      std::cerr << "could not bind() to: " << port << std::endl;
+      exit(1);
+   }
+
+   // Listen
+   if (listen(listener, 128) == -1) {
+      perror("listen() failed");
+      exit(1);
+   }
+
+   return listener;
+}
 
 /// Leader process that coordinates workers. Workers connect on the specified port
 /// and the coordinator distributes the work of the CSV file list.
@@ -15,15 +70,6 @@ int main(int argc, char* argv[]) {
       return 1;
    }
 
-   // TODO:
-   //    1. Allow workers to connect
-   //       socket(), bind(), listen(), accept(), see: https://beej.us/guide/bgnet/html/#system-calls-or-bust
-   //    2. Distribute the following work among workers
-   //       send() them some work
-   //    3. Collect all results
-   //       recv() the results
-   // Hint: Think about how you track which worker got what work
-
    auto curlSetup = CurlGlobalSetup();
 
    auto listUrl = std::string(argv[1]);
@@ -33,34 +79,113 @@ int main(int argc, char* argv[]) {
    curl.setUrl(listUrl);
    auto fileList = curl.performToStringStream();
 
-   size_t result = 0;
-   // Iterate over all files
-   for (std::string url; std::getline(fileList, url, '\n');) {
-      curl.setUrl(url);
-      // Download them
-      auto csvData = curl.performToStringStream();
-      for (std::string row; std::getline(csvData, row, '\n');) {
-         auto rowStream = std::stringstream(std::move(row));
+   std::vector<std::string> filesTodo;
+   filesTodo.reserve(100);
+   for (std::string url; std::getline(fileList, url, '\n');)
+      filesTodo.push_back(std::move(url));
 
-         // Check the URL in the second column
-         unsigned columnIndex = 0;
-         for (std::string column; std::getline(rowStream, column, '\t'); ++columnIndex) {
-            // column 0 is id, 1 is URL
-            if (columnIndex == 1) {
-               // Check if URL is "google.ru"
-               auto pos = column.find("://"sv);
-               if (pos != std::string::npos) {
-                  auto afterProtocol = std::string_view(column).substr(pos + 3);
-                  if (afterProtocol.starts_with("google.ru/"))
-                     ++result;
-               }
-               break;
+   // Listen
+   auto listener = getListenerSocket(argv[2]);
+
+   // Setup polling for new connections and worker responses
+   std::vector<pollfd> pollFds;
+   pollFds.push_back(pollfd{
+      .fd = listener,
+      .events = POLLIN,
+      .revents = {},
+   });
+
+   // Distribute the work
+   size_t result = 0;
+   auto distributedWork = std::unordered_map<int, std::string>();
+   while (!filesTodo.empty() || !distributedWork.empty()) {
+      poll(pollFds.data(), pollFds.size(), -1);
+      for (size_t index = 0, limit = pollFds.size(); index != limit; ++index) {
+         const auto& pollFd = pollFds[index];
+         // Look for ready connections
+         if (!(pollFd.revents & POLLIN)) continue;
+
+         auto assignWork = [&](int fd) {
+            // Assign work (if any)
+            if (filesTodo.empty()) return;
+            distributedWork[fd] = std::move(filesTodo.back());
+            filesTodo.pop_back();
+
+            const auto& file = distributedWork[fd];
+            if (auto status = send(fd, file.c_str(), file.size(), 0); status == -1) {
+               perror("send() failed");
+               filesTodo.push_back(std::move(distributedWork[fd]));
+               distributedWork.erase(fd);
             }
+         };
+
+         if (pollFd.fd == listener) {
+            // Incoming connection -> accept
+            auto addr = sockaddr();
+            socklen_t addrLen = sizeof(sockaddr);
+            auto newConnection = accept(listener, &addr, &addrLen);
+            if (newConnection == -1) {
+               perror("accept() failed");
+               continue;
+            }
+            pollFds.push_back(pollfd{
+               .fd = newConnection,
+               .events = POLLIN,
+               .revents = {},
+            });
+
+            // And directly assign some work
+            assignWork(newConnection);
+            continue;
          }
+
+         auto handleClientFailure = [&] {
+            // Worker failed. Make sure the work gets done by someone else.
+            if (distributedWork.contains(pollFd.fd)) {
+               filesTodo.push_back(std::move(distributedWork[pollFd.fd]));
+               distributedWork.erase(pollFd.fd);
+            }
+            // Drop the connection
+            close(pollFd.fd); // Bye!
+            std::swap(pollFds[index], pollFds.back());
+            pollFds.pop_back();
+            --index;
+            --limit;
+         };
+
+         // Client is ready -> recv result and send more work
+         auto buffer = std::array<char, 32>();
+         auto numBytes = recv(pollFd.fd, buffer.data(), buffer.size(), 0);
+         if (numBytes <= 0) {
+            if (numBytes < 0)
+               perror("recv() failed: ");
+            handleClientFailure();
+            continue;
+         }
+
+         // Parse response
+         auto response = std::string_view(buffer.data(), static_cast<size_t>(numBytes));
+         size_t clientResult = 0;
+         auto [_, ec] = std::from_chars(response.data(), response.data() + response.size(), clientResult);
+         if (ec != std::errc()) {
+            handleClientFailure();
+            continue;
+         }
+
+         // Result ok
+         result += clientResult;
+         distributedWork.erase(pollFd.fd);
+
+         // Assign more work
+         assignWork(pollFd.fd);
       }
    }
 
    std::cout << result << std::endl;
+
+   // Cleanup
+   for (auto& pollFd : pollFds)
+      close(pollFd.fd);
 
    return 0;
 }
--- worker.cpp
+++ worker.cpp
@@ -1,4 +1,39 @@
+#include "CurlEasyPtr.h"
+#include <array>
+#include <cstring>
 #include <iostream>
+#include <sstream>
+#include <thread>
+#include <netdb.h>
+#include <sys/socket.h>
+
+size_t processUrl(CurlEasyPtr& curl, std::string_view url) {
+   using namespace std::literals;
+   size_t result = 0;
+   // Download the file
+   curl.setUrl(std::string(url));
+   auto csvData = curl.performToStringStream();
+   for (std::string row; std::getline(csvData, row, '\n');) {
+      auto rowStream = std::stringstream(std::move(row));
+
+      // Check the URL in the second column
+      unsigned columnIndex = 0;
+      for (std::string column; std::getline(rowStream, column, '\t'); ++columnIndex) {
+         // column 0 is id, 1 is URL
+         if (columnIndex == 1) {
+            // Check if URL is "google.ru"
+            auto pos = column.find("://"sv);
+            if (pos != std::string::npos) {
+               auto afterProtocol = std::string_view(column).substr(pos + 3);
+               if (afterProtocol.starts_with("google.ru/"))
+                  ++result;
+            }
+            break;
+         }
+      }
+   }
+   return result;
+}
 
 /// Worker process that receives a list of URLs and reports the result
 /// Example:
@@ -10,16 +45,59 @@ int main(int argc, char* argv[]) {
       return 1;
    }
 
-   // TODO:
-   //    1. connect to coordinator specified by host and port
-   //       getaddrinfo(), connect(), see: https://beej.us/guide/bgnet/html/#system-calls-or-bust
-   //    2. receive work from coordinator
-   //       recv(), matching the coordinator's send() work
-   //    3. process work
-   //       see coordinator.cpp
-   //    4. report result
-   //       send(), matching the coordinator's recv()
-   //    5. repeat
+   // Set up the connection
+   addrinfo hints = {};
+   hints.ai_family = AF_UNSPEC;
+   hints.ai_socktype = SOCK_STREAM;
+   addrinfo* coordinatorAddr = nullptr;
+   if (auto status = getaddrinfo(argv[1], argv[2], &hints, &coordinatorAddr); status != 0) {
+      std::cerr << "getaddrinfo() failed: " << gai_strerror(status) << std::endl;
+      return 1;
+   }
 
+   // Try to connect to coordinator
+   int connection, status;
+   for (unsigned i = 0; i < 10; ++i) {
+      for (auto iter = coordinatorAddr; iter; iter = iter->ai_next) {
+         connection = socket(iter->ai_family, iter->ai_socktype, iter->ai_protocol);
+         if (connection == -1) {
+            std::cerr << "socket() failed: " << strerror(connection) << std::endl;
+            return 1;
+         }
+         status = connect(connection, iter->ai_addr, iter->ai_addrlen);
+         if (status != -1)
+            goto breakConnect;
+         close(connection);
+      }
+      std::this_thread::sleep_for(std::chrono::milliseconds(500));
+   }
+breakConnect:
+   freeaddrinfo(coordinatorAddr);
+   if (status == -1) {
+      perror("connect() failed");
+      return 1;
+   }
+
+   // Connected
+   auto curlSetup = CurlGlobalSetup();
+   auto curl = CurlEasyPtr::easyInit();
+   auto buffer = std::array<char, 1024>();
+   while (true) {
+      auto numBytes = recv(connection, buffer.data(), buffer.size(), 0);
+      if (numBytes <= 0) {
+         // connection closed / error
+         break;
+      }
+      auto url = std::string_view(buffer.data(), static_cast<size_t>(numBytes));
+      auto result = processUrl(curl, url);
+
+      auto response = std::to_string(result);
+      if (send(connection, response.c_str(), response.size(), 0) == -1) {
+         perror("send() failed");
+         break;
+      }
+   }
+
+   close(connection);
    return 0;
 }
